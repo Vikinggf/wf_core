cc9a891abfaf701913bee6622616993fa67f1444
zhuweifeng <zhuweifeng@jddfun.com>
2017-11-24 16:11:22 +0800

    [*]调整traceId打印顺序

--- src/main/java/com/wf/core/utils/MVCExceptionHandle.java
+++ src/main/java/com/wf/core/utils/MVCExceptionHandle.java
@@ -46,24 +46,24 @@ public class MVCExceptionHandle {
             }
             sb.delete(sb.length() - 1, sb.length());
             String s = sb.toString();
-            logger.error("traceId:{}, 参数非法：-{}-{}", traceId, s, ExceptionUtils.getStackTrace(e));
+            logger.error("参数非法：-{}-{}, traceId:{}", s, ExceptionUtils.getStackTrace(e), traceId);
             return new ErrorRspBean(400, s);
         } else if (e instanceof BaseController.LbmOAuthException) {
-            logger.error("traceId:{}, 用户没有登录:{}", traceId, ExceptionUtils.getStackTrace(e));
+            logger.error("用户没有登录:{}, traceId:{}", ExceptionUtils.getStackTrace(e), traceId);
             return new ErrorRspBean(401, "用户没有登录");
         } else if (e instanceof BaseController.ChannelErrorException) {
-            logger.error("traceId:{}, 渠道不存在或被禁用:{}", traceId, ExceptionUtils.getStackTrace(e));
+            logger.error("渠道不存在或被禁用:{}, traceId:{}", ExceptionUtils.getStackTrace(e), traceId);
             return new ErrorRspBean(402, "渠道不存在或被禁用");
         } else if (e instanceof HttpMessageNotReadableException) {
             HttpMessageNotReadableException le = (HttpMessageNotReadableException) e;
-            logger.error("traceId:{}, 请传入body:{}", traceId, ExceptionUtils.getStackTrace(le));
+            logger.error("请传入body:{}, traceId:{}", ExceptionUtils.getStackTrace(le), traceId);
             return new ErrorRspBean(400, "请传入body");
         } else if (e instanceof BusinessCommonException) {
             BusinessCommonException le = (BusinessCommonException) e;
-            logger.error("traceId:{}, 业务异常:{}", traceId, ExceptionUtils.getStackTrace(le));
+            logger.error("业务异常:{}, traceId:{}", ExceptionUtils.getStackTrace(le), traceId);
             return new ErrorRspBean(le.getCode(), le.getMsg());
         } else if (e instanceof UnauthorizedException) {
-            logger.error("traceId:{}, {}", traceId, ExceptionUtils.getStackTrace(e));
+            logger.error("{}, traceId:{}", ExceptionUtils.getStackTrace(e), traceId);
             Throwable t = e.getCause();
             if (t != null) {
                 logger.info(t.getMessage());
@@ -71,12 +71,12 @@ public class MVCExceptionHandle {
             return new ErrorRspBean(403, "禁止访问");
         } else if (e instanceof ConstraintViolationException) {
             List<String> list = BeanValidators.extractMessage((ConstraintViolationException) e);
-            logger.error("traceId:{}, {}", traceId, list.toString() + ExceptionUtils.getStackTrace(e));
+            logger.error("{}, traceId:{}", list.toString() + ExceptionUtils.getStackTrace(e), traceId);
             return new ErrorRspBean(400, list.toString());
         } else if (e instanceof HttpClientException) {
             return new ErrorRspBean(400, e.getMessage());
         } else {
-            logger.error("traceId:{}, 系统异常:{}", traceId, ExceptionUtils.getStackTrace(e));
+            logger.error("系统异常:{}, traceId:{}", ExceptionUtils.getStackTrace(e), traceId);
             return new ErrorRspBean(500, "系统异常");
         }
     }
